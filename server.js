93b20e5634d6bc539e8857dd7c2d10e78b7b4085
coder1414 <123735464+coder1414@users.noreply.github.com>
2024-09-05 18:34:49 +0530

    Update server.js added correct admin url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json()); // Parse incoming JSON requests
 // Allow requests from multiple origins
 const allowedOrigins = [
   'https://food-frontend-blond.vercel.app',  // Frontend URL
-  'https://your-admin-url.onrender.com',  // Admin URL
+  'https://admin-uyxr.onrender.com',  // Admin URL
   'https://food-backend-62oi.onrender.com'  // Backend URL
 ];
 
